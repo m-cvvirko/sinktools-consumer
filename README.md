6df972278cb31488f1fcf04e14694ac7facc64c6
m-cvvirko <marek@cvvirko.pl>
2024-05-09 17:23:13 +0200

    correction for composer plus sink connector, WIP

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # sinktools-consumer
 
 docker exec -it kafka kafka-topics --list --bootstrap-server localhost:9092
+
+docker-compose -f ./docker-compose.yml config
+docker-compose -f ./docker-compose.yml up --no-start
--- scripts/create-topics.sh
+++ scripts/create-topics.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 # Create topics
-kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1 --topic example-topic
+#kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1 --topic example-topic
+
+sleep 10
+# Create topics
+IFS=',' read -ra ADDR <<< "$KAFKA_CREATE_TOPICS"
+for i in "${ADDR[@]}"; do
+  kafka-topics.sh --create --bootstrap-server localhost:9092 --partitions ${i%:*:*} --replication-factor ${i##*:} --topic ${i%%:*}
+done
